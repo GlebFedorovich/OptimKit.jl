8aefef7858c8b2574edc326a3e71487de8dd6c87
GlebFedorovich <36506433+GlebFedorovich@users.noreply.github.com>
2024-01-19 13:35:50 +0100

    Update linesearches.jl

--- src/linesearches.jl
+++ src/linesearches.jl
@@ -187,6 +187,7 @@ function Base.iterate(iter::HagerZhangLineSearchIterator)
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, true)
     elseif (b.α - a.α) < eps(one(a.α))
         @warn "Linesearch bracket converged to a point without satisfying Wolfe conditions?"
+        a.α = 1e-5;
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, true)
     else
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, false)
