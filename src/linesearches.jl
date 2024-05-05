4098c0901cf67781a06a8ea33f04820933355483
GlebFedorovich <36506433+GlebFedorovich@users.noreply.github.com>
2024-05-05 19:16:36 +0200

    Update linesearches.jl

--- src/linesearches.jl
+++ src/linesearches.jl
@@ -187,7 +187,7 @@ function Base.iterate(iter::HagerZhangLineSearchIterator)
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, true)
     elseif (b.α - a.α) < eps(one(a.α))
         @warn "Linesearch bracket converged to a point without satisfying Wolfe conditions?"
-        throw(DomainError(ch, "Linesearch bracket converged to a point without satisfying Wolfe conditions?"))
+        throw(DomainError(1, "Linesearch bracket converged to a point without satisfying Wolfe conditions?"))
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, true)
     else
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, false)
@@ -235,7 +235,7 @@ function Base.iterate(iter::HagerZhangLineSearchIterator, state::Tuple{LineSearc
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, true)
     elseif (b.α - a.α) < eps(one(a.α))
         @warn "Linesearch bracket converged to a point without satisfying Wolfe conditions?"
-        throw(DomainError(ch, "Linesearch bracket converged to a point without satisfying Wolfe conditions?"))
+        throw(DomainError(1, "Linesearch bracket converged to a point without satisfying Wolfe conditions?"))
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, true)
     else
         return (a.x, a.f, a.∇f, a.ξ, a.α, a.dϕ), (a, b, numfg, false)
